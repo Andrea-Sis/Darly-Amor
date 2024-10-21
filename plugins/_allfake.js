a42da854c5a180800e1e6f3eec6c8db344d6e842
Andrea-Sis <andreaa.valen21@gmail.com>
2024-10-21 17:42:31 -0400

    Update _allfake.js

--- plugins/_allfake.js
+++ plugins/_allfake.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 export async function before(m, { conn }) {
 //let img = await (await fetch(`https://tinyurl.com/2c5hk765`)).buffer()
 let img = catalogo
- global.rcanal = {
+ global.fake = {
     contextInfo: {
     	isForwarded: true,
     forwardedNewsletterMessageInfo: {
@@ -45,4 +45,24 @@ let img = catalogo
 				}
 			}
 		}
+
+global.rcanal = {
+contextInfo: {
+isForwarded: true,
+forwardedNewsletterMessageInfo: {
+newsletterJid: "12098765423@newsletter",
+serverMessageId: 100,
+newsletterName: '𝑺𝑰𝑺𝑲𝑬𝑫-𝑩𝑶𝑻',
+},
+externalAdReply: { 
+showAdAttribution: true,
+title: '𝑺𝑰𝑺𝑲𝑬𝑫-𝑩𝑶𝑻',
+body: 'Super Bot de WhatsApp ⭐',
+previewType: "PHOTO",
+thumbnailUrl: 'https://i.ibb.co/qBxdwZW/file.jpg',
+sourceUrl: 'https://www.instagram.com/sisked_1',
+mediaType: 1,
+renderLargerThumbnail: false
+},},}
+	
 }
